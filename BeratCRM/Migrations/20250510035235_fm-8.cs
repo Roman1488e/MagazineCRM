f3c721f82460e7c4573377dcc05770c17403b813
Roman1488e <vertushkinroman@gmail.com>
2025-05-11 03:25:25 -0400

    migrations

---
+++ BeratCRM/Migrations/20250510035235_fm-8.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BeratCRM.Migrations
+{
+    /// <inheritdoc />
+    public partial class fm8 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isPaid",
+                table: "Debts",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isPaid",
+                table: "Debts");
+        }
+    }
+}
