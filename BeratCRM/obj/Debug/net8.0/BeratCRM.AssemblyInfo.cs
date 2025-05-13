1f37373a5bc6aa179bf2b8a77f0a26033ab7a1d6
Roman1488e <vertushkinroman@gmail.com>
2025-05-13 21:22:01 +0500

    publish

--- BeratCRM/obj/Debug/net8.0/BeratCRM.AssemblyInfo.cs
+++ BeratCRM/obj/Debug/net8.0/BeratCRM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BeratCRM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+067640998795fddde4816607c44079f3e708f090")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a1341380dd7d3de53a510943b019518c4bec261")]
 [assembly: System.Reflection.AssemblyProductAttribute("BeratCRM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BeratCRM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
